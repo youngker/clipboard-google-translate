e3f0e463ed434712b96474e4fb3b4fc3b1fdcaa4
YoungJoo Lee <youngker@gmail.com>
2023-01-28 11:45:24 +0900

    add flake.nix & update cargo

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+          buildInputs = with pkgs; [ cmake ]
+            ++ lib.optionals pkgs.stdenv.isDarwin
+            (with darwin.apple_sdk.frameworks; [ Carbon Cocoa Kernel ]);
+        };
+
+        devShell = with pkgs; mkShell {
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+
+        formatter = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
+      });
+}
